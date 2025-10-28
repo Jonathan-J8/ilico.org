a61efbaa1c4b82e8d900a74813af5722ed179662
jonathan <jattar02@gmail.com>
2025-10-28 14:35:34 +0100

    feat: setup form

--- app/src/form.ts
+++ app/src/form.ts
@@ -1,1 +1,27 @@
+const buttons = document.querySelectorAll('button[data-form]') as NodeListOf<HTMLButtonElement>;
+const select = document.querySelector('select') as HTMLSelectElement;
+const mail = document.querySelector('input[type="email"]') as HTMLInputElement;
+
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const formId = button.getAttribute('data-form');
+		select.selectedIndex = parseInt(formId || '0', 10);
+	});
+});
+
 const form = document.querySelector('form') as HTMLFormElement;
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const formData = new FormData(form);
+	formData.append('offre', select.value);
+	formData.append('email', mail.value);
+
+	// fetch(form.action, {
+	// 	method: form.method,
+	// 	body: formData,
+	// }).catch((error) => {
+	// 	console.error('Error submitting form:', error);
+	// 	alert('An error occurred while submitting the form.');
+	// });
+});
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,33 +1,35 @@
 import { Feed, PixelateImage, PixelateVideos, ScrambleText } from 'ilico-components';
-import animateHeader from './animateHeader';
-import { frames } from './globals';
+import { Animator } from 'joeat-utils';
 import './style.css';
 
-PixelateVideos.frames = frames;
-ScrambleText.frames = frames;
-frames.play();
+const animator = new Animator();
+PixelateVideos.frames = animator;
+ScrambleText.frames = animator;
 
 customElements.define(ScrambleText.name, ScrambleText);
 customElements.define(PixelateVideos.name, PixelateVideos);
 customElements.define(PixelateImage.name, PixelateImage);
 customElements.define(Feed.name, Feed);
-animateHeader();
 
 document.querySelector('#theme-controller > input')?.addEventListener('change', (e) => {
-	// const el = document.querySelector('#theme-controller > span') as HTMLElement;
-	// if (!el) return;
 	const { checked } = e.target as HTMLInputElement;
-
 	const theme = checked ? 'dark' : 'light';
 	document.documentElement.setAttribute('data-theme', theme);
-
-	// el.innerText = toggle.checked ? 'Dark' : 'Light';
 });
-// (async () => {
-//     const url = new URL(window.location.href);
-//     const debug = url.searchParams.has('debug') ? true : false;
-//     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-//     const webgl = (await import('pkg-webgl')).default;
-//     const dispose = webgl({canvas, debug, controls: false});
-//     import.meta.hot?.dispose(dispose);
-// })();
+
+(async () => {
+	try {
+		import('./header');
+		import('./form');
+		animator.play();
+	} catch (e) {
+		console.error('Error loading modules:', e);
+	}
+
+	// const url = new URL(window.location.href);
+	// const debug = url.searchParams.has('debug') ? true : false;
+	// const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+	// const webgl = (await import('pkg-webgl')).default;
+	// const dispose = webgl({canvas, debug, controls: false});
+	// import.meta.hot?.dispose(dispose);
+})();
