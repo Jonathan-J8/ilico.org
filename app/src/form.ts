f20b93835802454675de8297c2513e8dd0b295b4
jonathan <jattar02@gmail.com>
2025-10-28 14:58:31 +0100

    feat: form

--- app/src/form.ts
+++ app/src/form.ts
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll('button[data-form]') as NodeListOf<HTMLButtonElement>;
 const select = document.querySelector('select') as HTMLSelectElement;
 const mail = document.querySelector('input[type="email"]') as HTMLInputElement;
+const output = document.querySelector('p[data-form="output"]') as HTMLParagraphElement;
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
@@ -17,11 +18,20 @@ form.addEventListener('submit', (e) => {
 	formData.append('offre', select.value);
 	formData.append('email', mail.value);
 
-	// fetch(form.action, {
-	// 	method: form.method,
-	// 	body: formData,
-	// }).catch((error) => {
-	// 	console.error('Error submitting form:', error);
-	// 	alert('An error occurred while submitting the form.');
-	// });
+	fetch(form.action, {
+		method: form.method,
+		body: formData,
+	})
+		.then((response) => {
+			if (!response.ok) throw new Error('Network response was not ok');
+			return response.text();
+		})
+		.then((text) => {
+			output.style.color = 'inherit';
+			output.textContent = text;
+		})
+		.catch((error) => {
+			output.style.color = 'red';
+			output.textContent = 'Erreur de soumission du formulaire : ' + error?.message || '';
+		});
 });
